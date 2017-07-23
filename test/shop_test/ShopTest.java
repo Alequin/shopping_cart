16bfbf21fab1eaa1722dbb9115adfe006f18769c
James Cox <j-cox03@hotmail.com>
2017-07-23 12:44:54 +0100

    ShopTest: made method for getting money and setting inital money in constructor

--- test/shop_test/ShopTest.java
+++ test/shop_test/ShopTest.java
@@ -4,23 +4,32 @@ import org.junit.Before;
 import org.junit.Test;
 import shop.Shop;
 
+import java.math.BigDecimal;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class ShopTest {
 
     private Shop shop1;
+    private static String INITAL_SHOP_NAME = "The Big Shop";
+    private static double INITAL_SHOP_MONEY = 500;
 
     @Before
     public void setup(){
-        shop1 = new Shop("The Big Shop");
+        shop1 = new Shop(INITAL_SHOP_NAME, INITAL_SHOP_MONEY);
     }
 
     @Test
     public void canGetName(){
-        String expected1 = "The Big Shop";
+        String expected1 = INITAL_SHOP_NAME;
         String result1 = shop1.getName();
         assertEquals(expected1, result1);
     }
 
-
+    @Test
+    public void canGetMoney(){
+        double expected1 = INITAL_SHOP_MONEY;
+        double result1 = shop1.getMoney();
+        assertEquals(expected1, result1);
+    }
 }
