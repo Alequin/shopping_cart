aec0fe0b1a61f782acc499f0ce06d9156190231d
James Cox <j-cox03@hotmail.com>
2017-07-23 18:37:33 +0100

    BasketTest: made test canAddToBasket

--- test/shop_test/BasketTest.java
+++ test/shop_test/BasketTest.java
@@ -1,19 +1,50 @@
 package shop_test;
 
+import Products.Crisps;
+import Products.Pizza;
+import Products.Product;
+import Products.ToothPaste;
 import org.junit.Before;
+import org.junit.Test;
 import shop.Basket;
 import shop.Shop;
+import shop.Stock;
+
+import static junit.framework.TestCase.assertEquals;
 
 
 public class BasketTest {
 
     private Shop shop1;
+    private Stock shopStock;
     private Basket basket1;
+    private Product[] products;
 
     @Before
     public void setup(){
         shop1 = new Shop("The Shop", 500);
+        shopStock = shop1.getStock();
         basket1 = shop1.makeBasket();
+
+        products = new Product[3];
+        products[0] = new Pizza(5);
+        products[1] = new Crisps(10);
+        products[2] = new ToothPaste(15);
+
+        for(Product product : products){
+            shopStock.add(product);
+        }
+    }
+
+    @Test
+    public void canAddToBasket(){
+        int preAddLength = basket1.countItems();
+
+        basket1.add(0);
+        basket1.add(0);
+        basket1.add(0);
+
+        assertEquals(preAddLength+3, basket1.countItems());
     }
 
 }
