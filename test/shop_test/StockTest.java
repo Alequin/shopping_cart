9bce87178a9b9ae2d52691d74866abe391ea867a
James Cox <j-cox03@hotmail.com>
2017-07-23 13:34:55 +0100

    StockTest: made test for adding, removing and counting items in stock

--- test/shop_test/StockTest.java
+++ test/shop_test/StockTest.java
@@ -1,5 +1,6 @@
 package shop_test;
 
+import Products.Pizza;
 import org.junit.Before;
 import org.junit.Test;
 import shop.Stock;
@@ -15,4 +16,12 @@ public class StockTest {
         stock1 = new Stock();
     }
 
+    @Test
+    public void canAddAndRemoveItem(){
+        Pizza pizza = new Pizza(6.50);
+        int preLength = stock1.countItems();
+        stock1.addItem(pizza);
+        assertEquals(preLength, stock1.countItems());
+        assertEquals(pizza, stock1.remove(0));
+    }
 }
