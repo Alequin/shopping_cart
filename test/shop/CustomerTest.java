ce21b3e3aa4ca84c5d7cb5d5c4934b40b12da325
James Cox <j-cox03@hotmail.com>
2017-07-23 22:44:16 +0100

    CustomerTest: made tests around money managment

--- test/shop/CustomerTest.java
+++ test/shop/CustomerTest.java
@@ -3,15 +3,42 @@ package shop;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class CustomerTest {
 
     private Customer customer;
 
-    @Test
+    @Before
     public void setup(){
         customer = new Customer(200.00, true);
+    }
+
+    @Test
+    public void canGetMoney(){
+        assertEquals(200.00, customer.getMoney());
+    }
+
+    @Test
+    public void canGetMoneyAsBigDecimal(){
+        BigDecimal money = customer.getMoneyAsBigDecimal();
+        assertEquals(200.00, money.doubleValue());
+    }
+
+    @Test
+    public void canIncreaseMoney(){
+        double amount = 100;
+        customer.increaseMoney(amount);
+        assertEquals(300.00, customer.getMoney());
+    }
+
+    @Test
+    public void canDecreaseMoney(){
+        double amount = 100;
+        customer.decreaseMoney(amount);
+        assertEquals(100.00, customer.getMoney());
     }
 
 }
