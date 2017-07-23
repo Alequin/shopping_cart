a9f5cb88f22be6a8a617b8403ce5b884f79f6e09
James Cox <j-cox03@hotmail.com>
2017-07-23 23:07:42 +0100

    CustomerTest: made test for letting a customer enter a shop

--- test/shop/CustomerTest.java
+++ test/shop/CustomerTest.java
@@ -9,35 +9,43 @@ import static junit.framework.TestCase.assertEquals;
 
 public class CustomerTest {
 
-    private Customer customer;
+    private Shop shop1
+    private Customer customer1;
 
     @Before
     public void setup(){
-        customer = new Customer(200.00, true);
+        shop1 = new Shop("The Shop", 1000);
+        customer1 = new Customer(200.00, true);
     }
 
     @Test
     public void canGetMoney(){
-        assertEquals(200.00, customer.getMoney());
+        assertEquals(200.00, customer1.getMoney());
     }
 
     @Test
     public void canGetMoneyAsBigDecimal(){
-        BigDecimal money = customer.getMoneyAsBigDecimal();
+        BigDecimal money = customer1.getMoneyAsBigDecimal();
         assertEquals(200.00, money.doubleValue());
     }
 
     @Test
     public void canIncreaseMoney(){
         double amount = 100;
-        customer.increaseMoney(amount);
-        assertEquals(300.00, customer.getMoney());
+        customer1.increaseMoney(amount);
+        assertEquals(300.00, customer1.getMoney());
     }
 
     @Test
     public void canDecreaseMoney(){
         double amount = 100;
-        customer.decreaseMoney(amount);
-        assertEquals(100.00, customer.getMoney());
+        customer1.decreaseMoney(amount);
+        assertEquals(100.00, customer1.getMoney());
+    }
+
+    @Test
+    public void customerCanEnterShop(){
+        customer1.enterShop(shop1);
+        assertEquals(true, shop1.isCustomerInShop(customer1));
     }
 }
