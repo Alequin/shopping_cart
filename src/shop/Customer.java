76d4af4f6fb00ab260f1b03bba4b7084090ca352
James Cox <j-cox03@hotmail.com>
2017-07-23 22:41:14 +0100

    Customer: made constructor and fields

--- src/shop/Customer.java
+++ src/shop/Customer.java
@@ -1,4 +1,18 @@
 package shop;
 
+import java.math.BigDecimal;
+
 public class Customer {
+
+    private BigDecimal money;
+    private boolean hasLoyaltyCard;
+
+    private Basket basket;
+    private Shop shop;
+
+    public Customer(double money, boolean hasLoyaltyCard){
+        this.money = BigDecimal.valueOf(money);
+        this.hasLoyaltyCard = hasLoyaltyCard;
+    }
+
 }
