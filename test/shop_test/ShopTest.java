0920ba64569b6557a82da04f9f4805c3d02ffa23
James Cox <j-cox03@hotmail.com>
2017-07-23 12:58:48 +0100

    ShopTest: made test to check if exception is thrown if money argument is less than zero

--- test/shop_test/ShopTest.java
+++ test/shop_test/ShopTest.java
@@ -4,32 +4,43 @@ import org.junit.Before;
 import org.junit.Test;
 import shop.Shop;
 
-import java.math.BigDecimal;
-
 import static junit.framework.TestCase.assertEquals;
 
 public class ShopTest {
 
     private Shop shop1;
-    private static String INITAL_SHOP_NAME = "The Big Shop";
-    private static double INITAL_SHOP_MONEY = 500;
+    private static String INITIAL_SHOP_NAME = "The Big Shop";
+    private static double INITIAL_SHOP_MONEY = 500;
 
     @Before
     public void setup(){
-        shop1 = new Shop(INITAL_SHOP_NAME, INITAL_SHOP_MONEY);
+        shop1 = new Shop(INITIAL_SHOP_NAME, INITIAL_SHOP_MONEY);
     }
 
     @Test
     public void canGetName(){
-        String expected1 = INITAL_SHOP_NAME;
+        String expected1 = INITIAL_SHOP_NAME;
         String result1 = shop1.getName();
         assertEquals(expected1, result1);
     }
 
     @Test
     public void canGetMoney(){
-        double expected1 = INITAL_SHOP_MONEY;
+        double expected1 = INITIAL_SHOP_MONEY;
         double result1 = shop1.getMoney();
         assertEquals(expected1, result1);
+    }
+
+    @Test
+    public void canSetMoneyToZero(){
+        double expected1 = 0;
+        Shop shop = new Shop("x shop", 0);
+        double result1 = shop.getMoney();
+        assertEquals(expected1, result1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cannotSetInitialMoneyLessThanZero(){
+        new Shop("x shop", -0.1);
     }
 }
