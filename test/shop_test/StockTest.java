8e8e09909b83cacab1cd64879e4d12844b852b6f
James Cox <j-cox03@hotmail.com>
2017-07-23 14:34:38 +0100

    StockTest: refactored test canCountProducts

--- test/shop_test/StockTest.java
+++ test/shop_test/StockTest.java
@@ -33,34 +33,24 @@ public class StockTest {
         Product p1 = new Pizza(10);
         Product p2 = new Crisps(10);
         Product p3 = new ToothPaste(10);
+        Product[] products = {p1, p2, p3};
 
         int expected1 = 0;
         int result1 = stock1.countItems();
         assertEquals(expected1, result1);
 
-        stock1.add(p1);
-
-        int expected2 = expected1 + 1;
-        int result2 = stock1.countItems();
-        assertEquals(expected2, result2);
-
-        stock1.add(p2);
-
-        int expected3 = expected2 + 1;
-        int result3 = stock1.countItems();
-        assertEquals(expected3, result3);
-
-        stock1.add(p3);
-
-        int expected4 = expected3 + 1;
-        int result4 = stock1.countItems();
-        assertEquals(expected4, result4);
+        for(int j=0; j<3; j++){
+            stock1.add(products[j]);
+            expected1++;
+            result1 = stock1.countItems();
+            assertEquals(expected1, result1);
+        }
 
         stock1.add(p1);
         stock1.add(p2);
         stock1.add(p3);
 
         int result5 = stock1.countItems();
-        assertEquals(expected4, result5);
+        assertEquals(expected1, result5);
     }
 }
