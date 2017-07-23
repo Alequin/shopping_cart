1cabbeed2dfd482f294028e6b7db66c22db5a442
James Cox <j-cox03@hotmail.com>
2017-07-23 12:46:07 +0100

    Shop: made method to get money and added to constuctor to set money

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -1,15 +1,23 @@
 package shop;
 
+import java.math.BigDecimal;
+
 public class Shop {
 
     private String name;
+    private BigDecimal money;
 
-    public Shop(String name){
+    public Shop(String name, double money){
         this.name = name;
+        this.money = BigDecimal.valueOf(money);
     }
 
     public String getName(){
         return name;
+    }
+
+    public double getMoney(){
+        return money.doubleValue();
     }
 
 }
