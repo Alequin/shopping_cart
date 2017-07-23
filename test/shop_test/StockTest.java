736e2f903e8f7104865f7c6a86581bcd0a633edc
James Cox <j-cox03@hotmail.com>
2017-07-23 13:25:41 +0100

    Made StockTest class

---
+++ test/shop_test/StockTest.java
@@ -0,0 +1,17 @@
+package shop_test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class StockTest {
+
+    private Stock stock1;
+
+    @Before
+    public void setup(){
+        stock1 = new Stock();
+    }
+
+}
